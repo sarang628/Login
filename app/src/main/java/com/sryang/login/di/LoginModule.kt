17662d2b3e83a797addcd7e060794a5f26cbacc2
Sarang Yang <sarang628@naver.com>
2022-08-20 16:15:52 +0900

    update

--- app/src/main/java/com/sryang/login/di/LoginModule.kt
+++ app/src/main/java/com/sryang/login/di/LoginModule.kt
@@ -1,10 +1,13 @@
-package com.sarang.toringlogin
+package com.sryang.login.di
 
 import android.content.Context
 import android.content.Intent
 import androidx.fragment.app.FragmentManager
 import com.example.torang_core.login.FacebookLoginProvider
 import com.example.torang_core.navigation.LoginNavigation
+import com.sarang.toringlogin.FacebookLoginProviderImpl
+import com.sarang.toringlogin.LoginManager
+import com.sarang.toringlogin.TorangLoginManager
 import com.sarang.toringlogin.login.LoginActivity
 import dagger.Binds
 import dagger.Module
