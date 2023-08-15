bfea3e031998c77043f62988e6a5ca0da183c15d
SRYANG <sarang628@naver.com>
2023-08-15 17:29:51 +0800

    remove xml base code

---
+++ library/src/main/java/com/sarang/toringlogin/login/LoginScreen.kt
@@ -0,0 +1,14 @@
+package com.sarang.toringlogin.login
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Preview
+@Composable
+fun LoginScreen() {
+    Column {
+        Text(text = "test")
+    }
+}
