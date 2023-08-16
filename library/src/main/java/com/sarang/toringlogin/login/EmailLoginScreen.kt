e3423c7830f4549495ede6ab995238b60327e751
SRYANG <sarang628@naver.com>
2023-08-16 17:57:42 +0800

    update

--- library/src/main/java/com/sarang/toringlogin/login/EmailLoginScreen.kt
+++ library/src/main/java/com/sarang/toringlogin/login/EmailLoginScreen.kt
@@ -1,16 +1,33 @@
 package com.sarang.toringlogin.login
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
-import com.sryang.library.LoginLogic
-import com.sryang.library.LoginResult
-import com.sryang.library.LogoutResult
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Preview
 @Composable
 fun EmailLoginScreen() {
     Column {
-
+        Text(text = "Email")
+        OutlinedTextField(
+            value = "",
+            onValueChange = { },
+            label = { Text("Email") }
+        )
+        Text(text = "Password")
+        OutlinedTextField(
+            value = "",
+            onValueChange = { },
+            label = { Text("Password") }
+        )
+        Button(onClick = { /*TODO*/ }) {
+            Text(text = "Login")
+        }
     }
 }
