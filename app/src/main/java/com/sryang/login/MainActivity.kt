39ad948062ad2ee77592fab36f19394c8da9a90b
SRYANG <sarang628@naver.com>
2023-08-21 18:04:30 +0800

    update

--- app/src/main/java/com/sryang/login/MainActivity.kt
+++ app/src/main/java/com/sryang/login/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.sryang.login
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import com.sarang.toringlogin.login.LoginLogicImpl
+import com.sryang.library.LoginLogic
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
 
+        setContent{
+            val loginLogic : LoginLogic = LoginLogicImpl()
+            loginLogic.LoginScreen()
+        }
+
+    }
 }
--- library/src/main/java/com/sarang/toringlogin/login/LoginLogicImpl.kt
+++ library/src/main/java/com/sarang/toringlogin/login/LoginLogicImpl.kt
@@ -1,18 +1,7 @@
 package com.sarang.toringlogin.login
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.sryang.library.LoginLogic
 import com.sryang.library.LoginResult
 import com.sryang.library.LogoutResult
@@ -22,7 +11,7 @@ class LoginLogicImpl : LoginLogic {
     @Preview
     @Composable
     override fun LoginScreen() {
-        LoginScreen()
+        com.sarang.toringlogin.login.LoginScreen()
     }
 
     override suspend fun requestLogin(email: String, password: String, loginResult: LoginResult) {
