3dec9d92e376c1e9e7e1bcf67c84ffd7078f100d
SRYANG <sarang628@naver.com>
2023-08-27 14:17:12 +0800

    update

--- library/src/main/java/com/sarang/toringlogin/login/LoginScreen.kt
+++ library/src/main/java/com/sarang/toringlogin/login/LoginScreen.kt
@@ -1,46 +1,67 @@
 package com.sarang.toringlogin.login
 
 import android.widget.Toast
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.sarang.theme.R
 import com.sryang.torang_repository.services.impl.LoginServiceTest
 import com.sryang.torang_repository.services.impl.getLoginService
+import kotlinx.coroutines.delay
 
 @Composable
 internal fun LoginScreen1(
     onClickFacebookLogin: (Int) -> Unit,
     onClickEmail: (Int) -> Unit
 ) {
-    Column(modifier = Modifier.fillMaxSize()) {
-        Row(
-            Modifier
-                .fillMaxWidth()
-                .padding(top = 100.dp),
-            horizontalArrangement = Arrangement.Center
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(colorResource(id = R.color.colorSecondaryLight))
+    ) {
+        Spacer(modifier = Modifier.height(100.dp))
+        Column(
+            Modifier.fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Text(text = "TORANG", fontSize = 25.sp)
+            Text(
+                text = "T O R A N G",
+                color = colorResource(id = R.color.colorSecondary),
+                fontSize = 45.sp,
+                fontWeight = FontWeight.Bold
+            )
         }
-        Row(
-            Modifier
-                .fillMaxWidth()
-                .padding(top = 80.dp), horizontalArrangement = Arrangement.Center
+        Spacer(modifier = Modifier.height(30.dp))
+        Column(
+            Modifier.fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Text(text = "Hit the spot")
+            Text(
+                text = "Hit the spot",
+                color = colorResource(id = R.color.colorSecondary),
+                fontSize = 20.sp
+            )
         }
         Row(
             Modifier
@@ -84,7 +105,7 @@ fun LoginScreen() {
         }
         composable("emailLogin") {
             EmailLoginScreen(
-                onLogin = {  },
+                onLogin = { },
                 loginService = getLoginService(LocalContext.current)
             )
         }
