7122d71a4d91c76b657f8a191b40e63be1c21071
SRYANG <sarang628@naver.com>
2023-09-02 16:34:09 +0800

    update

--- library/src/main/java/com/sarang/toringlogin/login/LoginLogicImpl.kt
+++ library/src/main/java/com/sarang/toringlogin/login/LoginLogicImpl.kt
@@ -1,17 +1,14 @@
 package com.sarang.toringlogin.login
 
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
 import com.sryang.library.LoginLogic
 import com.sryang.library.LoginResult
 import com.sryang.library.LogoutResult
 
 class LoginLogicImpl : LoginLogic {
 
-    @Preview
     @Composable
     override fun LoginScreen() {
-        com.sarang.toringlogin.login.LoginScreen()
     }
 
     override suspend fun requestLogin(email: String, password: String, loginResult: LoginResult) {
