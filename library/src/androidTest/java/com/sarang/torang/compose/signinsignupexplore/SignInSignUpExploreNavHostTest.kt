ed811e86704ef3044a14ba987555b2cbf3e1587d
SRYANG <sarang628@naver.com>
2023-12-19 15:43:44 +0800

    add SignInSignUpExploreNavHostTest

--- library/src/androidTest/java/com/sarang/torang/compose/signinsignupexplore/SignInSignUpExploreNavHostTest.kt
+++ library/src/androidTest/java/com/sarang/torang/compose/signinsignupexplore/SignInSignUpExploreNavHostTest.kt
@@ -3,16 +3,15 @@ package com.sarang.torang.compose.signinsignupexplore
 import androidx.activity.ComponentActivity
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
-import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithContentDescription
 import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performClick
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.sarang.torang.R
+import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
-import com.sarang.torang.R
-import org.junit.Before
 
 @RunWith(AndroidJUnit4::class)
 class SignInSignUpExploreNavHostTest {
