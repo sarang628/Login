d8334850c2a644d69cef18aa00e289c34f665b25
sryang <sarang628@gmail.com>
2024-08-09 22:25:35 +0800

    update settings.kts

---
+++ settings.gradle.kts
@@ -0,0 +1,26 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+    plugins {
+        id("org.jetbrains.kotlin.jvm") version "1.8.20"
+    }
+}
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.5.0"
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        maven("https://jitpack.io")
+        maven("https://devrepo.kakao.com/nexus/content/groups/public/")
+    }
+}
+
+rootProject.name = "Login"
+include(":app")
+include(":library")
